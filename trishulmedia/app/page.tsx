12028df324ffb786160fd0f273aeb3bd5e156539
David Wilons <nadardavidwilson@gmail.com>
2026-09-04 19:54:32 +0530

    Test 4

--- trishulmedia/app/page.tsx
+++ trishulmedia/app/page.tsx
@@ -24,6 +24,9 @@ export default function Home() {
           <p className="max-w-2xl text-lg leading-8 text-slate-700">
           </p>
         </div>
+        <p>
+          David Wilson
+        </p>
 
 
         
